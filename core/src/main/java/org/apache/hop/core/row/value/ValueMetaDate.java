38b30bcda3b403083b0c37685209a34c9fbc8dfb
nadment <nadment@gmail.com>
2020-03-16 22:11:41 +0100

    HOP-107
    * Restore removed constructor ValueMetaDate

--- core/src/main/java/org/apache/hop/core/row/value/ValueMetaDate.java
+++ core/src/main/java/org/apache/hop/core/row/value/ValueMetaDate.java
@@ -41,9 +41,9 @@ public class ValueMetaDate extends ValueMetaBase implements ValueMetaInterface {
     super( name, type );
   }
 
-//  public ValueMetaDate( String name, int length, int precision ) {
-//    super( name, ValueMetaInterface.TYPE_DATE, length, precision );
-//  }
+  public ValueMetaDate( String name, int length, int precision ) {
+    super( name, ValueMetaInterface.TYPE_DATE, length, precision );
+  }
 
   @Override
   public Object getNativeDataType( Object object ) throws HopValueException {
