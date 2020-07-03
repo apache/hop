9b924a9090a078307d07f66bc991a96bdfe33d43
Hiromu Hota <hiromu.hota@hal.hitachi.com>
2020-07-03 04:34:21 -0700

    HOP-13: porting HiromuHota/pentaho-kettle#99 (#216)
    Related to
    https://github.com/HiromuHota/rap/commit/cc13fbdb6d6d537747f7fb
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=529704#c5

--- engine/src/main/java/org/apache/hop/workflow/WorkflowPainter.java
+++ engine/src/main/java/org/apache/hop/workflow/WorkflowPainter.java
@@ -74,7 +74,11 @@ public class WorkflowPainter extends BasePainter<WorkflowHopMeta, ActionCopy> {
     Point thumb = getThumb( area, max );
     offset = getOffset( thumb, area );
 
+    // Make sure the canvas is scaled 100%
+    gc.setTransform( 0.0f, 0.0f, 1.0f );
+    // First clear the image in the background color
     gc.setBackground( EColor.BACKGROUND );
+    gc.fillRectangle( 0, 0, area.x, area.y );
 
     if ( hori != null ) {
       hori.setThumb( thumb.x );
