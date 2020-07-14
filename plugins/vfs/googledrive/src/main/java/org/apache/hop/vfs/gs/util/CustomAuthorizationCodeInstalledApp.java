cec6d24f791bd34dcdbd680ca4d780096a8d5914
Matt Casters <mattcasters@gmail.com>
2020-07-14 16:12:23 +0200

    HOP-536 license header fixes and RAT maven plugin added

--- plugins/vfs/googledrive/src/main/java/org/apache/hop/vfs/gs/util/CustomAuthorizationCodeInstalledApp.java
+++ plugins/vfs/googledrive/src/main/java/org/apache/hop/vfs/gs/util/CustomAuthorizationCodeInstalledApp.java
@@ -1,5 +1,7 @@
 /*!
-* Copyright (C) 2017 by Hitachi Vantara : http://www.pentaho.com
+* Hop : The Hop Orchestration Platform
+ *
+ * http://www.project-hop.org
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
