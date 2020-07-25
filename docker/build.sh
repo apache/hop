df4ad38e369e73fd533647ede2612d21a591bce3
Hiromu Hota <hiromu.hota@hal.hitachi.com>
2020-07-24 22:28:01 -0700

    Update version to 0.40-SNAPSHOT

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cp ../assemblies/web/target/hop.war ./
-cp ../assemblies/plugins/dist/target/hop-assemblies-plugins-dist-0.30-SNAPSHOT.zip ./hop-assemblies-plugins-dist.zip
+cp ../assemblies/plugins/dist/target/hop-assemblies-plugins-dist-*.zip ./hop-assemblies-plugins-dist.zip
 docker build -t project-hop .
