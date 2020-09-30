a38ce7ba4d81914af9d7df84e4ca204d84e1d0e6
Brandon Jackson <usbrandon@gmail.com>
2020-09-30 15:44:57 -0500

    setlocal added to prevent variables set when running the first time to interfere with running a second time in the same environment (command prompt session)
    Added "DEBUG" command to dynamically add the parameters to the startup so no editing of the script is required.
    Added a "help" section that describes what the parameters do and provides a real life example of how to run the command completely.
    Added hop-conf.bat

--- assemblies/static/src/main/resources/hop-gui.bat
+++ assemblies/static/src/main/resources/hop-gui.bat
@@ -1,37 +1,71 @@
+echo off
 setlocal
 set LIBSPATH=lib
 set SWTJAR=libswt\win64
 
+set _temphelp=0
+if [%1]==[help] set _temphelp=1
+if [%1]==[Help] set _temphelp=1
+if %_temphelp%==1 (GOTO Help) ELSE (GOTO NormalStart)
+
+:Help
+echo ===[Hop Help - hop-gui.bat]===============================================
+echo Normally, no parameters are required to start Hop.  There is a debug mode
+echo that you can start by passing in DEBUG as the first parameter after hop-gui.bat
+echo.
+echo Example:
+echo   hop-gui.bat DEBUG
+echo
+echo The debug mode opens port 5005 locally when Hop starts allowing you to attaching
+echo a debugger from your favorite Java IDE tool and step code.
+echo ==========================================================================
+GOTO End
+
+:NormalStart
 REM set java primary is HOP_JAVA_HOME fallback to JAVA_HOME or default java
 if not "%HOP_JAVA_HOME%"=="" (
-    set _HOP_JAVA="%HOP_JAVA_HOME%\bin\java"
+    set _HOP_JAVA="%HOP_JAVA_HOME%bin\java"
 ) else if not "%JAVA_HOME%"=="" (
-    set _HOP_JAVA="%JAVA_HOME%\bin\java"
+    set _HOP_JAVA="%JAVA_HOME%bin\java"
 ) else (
-    set _HOP_JAVA="java"
+    set _HOP_JAVA=java
 )
 
 REM # Settings for all OSses
 
-if "%HOP_OPTIONS%"=="" set HOP_OPTIONS="-Xmx2048m"
+if "%HOP_OPTIONS%"=="" set HOP_OPTIONS=-Xmx2048m
 
+REM
+REM If the user passes in DEBUG as the first parameter, it starts Hop in debugger mode and opens port 5005
+REM to allow attaching a debugger to step code.
+if [%1]==[DEBUG] (
 REM # optional line for attaching a debugger
-REM set HOP_OPTIONS=%HOP_OPTIONS% -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005
+set HOP_OPTIONS=%HOP_OPTIONS% -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005)
 
 REM Pass HOP variables if they're set.
 if not "%HOP_AUDIT_FOLDER%"=="" (
-  set HOP_OPTIONS=%HOP_OPTIONS% "-DHOP_AUDIT_FOLDER="%HOP_AUDIT_FOLDER%
+  set HOP_OPTIONS=%HOP_OPTIONS% -DHOP_AUDIT_FOLDER=%HOP_AUDIT_FOLDER%
 )
 if not "%HOP_CONFIG_FOLDER%"=="" (
-  set HOP_OPTIONS=%HOP_OPTIONS% "-DHOP_CONFIG_FOLDER="%HOP_CONFIG_FOLDER%
+  set HOP_OPTIONS=%HOP_OPTIONS% -DHOP_CONFIG_FOLDER=%HOP_CONFIG_FOLDER%
 )
 if not "%HOP_SHARED_JDBC_FOLDER%"=="" (
-  set HOP_OPTIONS=%HOP_OPTIONS% "-DHOP_SHARED_JDBC_FOLDER="%HOP_SHARED_JDBC_FOLDER%
+  set HOP_OPTIONS=%HOP_OPTIONS% -DHOP_SHARED_JDBC_FOLDER=%HOP_SHARED_JDBC_FOLDER%
 )
 
-set HOP_OPTIONS=%HOP_OPTIONS% "-DHOP_PLATFORM_OS=Windows"
-set HOP_OPTIONS=%HOP_OPTIONS% "-DHOP_PLATFORM_RUNTIME=GUI"
+set HOP_OPTIONS=%HOP_OPTIONS% -DHOP_PLATFORM_OS=Windows
+set HOP_OPTIONS=%HOP_OPTIONS% -DHOP_PLATFORM_RUNTIME=GUI
+echo ===[Environment Settings - hop-gui.bat]===================================
+echo.
+echo Java identified as %_HOP_JAVA%
+echo.
+echo HOP_OPTIONS=%HOP_OPTIONS%
+echo.
+echo Command to start Hop will be:
+echo %_HOP_JAVA% -classpath %LIBSPATH%\*;%SWTJAR%\* -Djava.library.path=%LIBSPATH% %HOP_OPTIONS% org.apache.hop.ui.hopgui.HopGui
+echo.
+echo ===[Starting Hop]=========================================================
 
-@echo on
-%_HOP_JAVA% -classpath %LIBSPATH%\*;%SWTJAR%\* "-Djava.library.path=%LIBSPATH%" %HOP_OPTIONS% org.apache.hop.ui.hopgui.HopGui
+%_HOP_JAVA% -classpath %LIBSPATH%\*;%SWTJAR%\* -Djava.library.path=%LIBSPATH% %HOP_OPTIONS% org.apache.hop.ui.hopgui.HopGui
 @echo off
+:End
