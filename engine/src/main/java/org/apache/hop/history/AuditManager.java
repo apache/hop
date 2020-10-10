8f2ccba981bf6908fa0114711eebea2971135960
Mahendran Mookkiah <mahendran.mookkiah@gmail.com>
2020-10-10 08:22:28 -0400

    Just formatting using spotless and google-java-format

--- engine/src/main/java/org/apache/hop/history/AuditManager.java
+++ engine/src/main/java/org/apache/hop/history/AuditManager.java
@@ -19,12 +19,7 @@
  * limitations under the License.
  *
  ******************************************************************************/
-
 package org.apache.hop.history;
-
-import org.apache.hop.core.exception.HopException;
-import org.apache.hop.core.logging.ILogChannel;
-import org.apache.hop.history.local.LocalAuditManager;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -32,94 +27,104 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import org.apache.hop.core.exception.HopException;
+import org.apache.hop.core.logging.ILogChannel;
+import org.apache.hop.history.local.LocalAuditManager;
 
 public class AuditManager {
 
-  private static AuditManager instance;
+    private static AuditManager instance;
 
-  private IAuditManager activeAuditManager;
+    private IAuditManager activeAuditManager;
 
-  private AuditManager() {
-    activeAuditManager = new LocalAuditManager();
-  }
-
-  public static final AuditManager getInstance() {
-    if (instance==null) {
-      instance = new AuditManager();
+    private AuditManager() {
+        activeAuditManager = new LocalAuditManager();
     }
-    return instance;
-  }
 
-  public static final IAuditManager getActive() {
-    return getInstance().getActiveAuditManager();
-  }
-
-  /**
-   * Gets activeAuditManager
-   *
-   * @return value of activeAuditManager
-   */
-  public IAuditManager getActiveAuditManager() {
-    return activeAuditManager;
-  }
-
-  /**
-   * @param activeAuditManager The activeAuditManager to set
-   */
-  public void setActiveAuditManager( IAuditManager activeAuditManager ) {
-    this.activeAuditManager = activeAuditManager;
-  }
-
-  // Convenience methods...
-  //
-  public static final void registerEvent( String group, String type, String name, String operation ) throws HopException {
-    getActive().storeEvent( new AuditEvent( group, type, name, operation, new Date() ) );
-  }
-
-  public static final List<AuditEvent> findEvents( String group, String type, String operation, int maxNrEvents, boolean unique ) throws HopException {
-    List<AuditEvent> events = getActive().findEvents( group, type, unique );
-    Set<String> names = new HashSet<>();
-
-    if ( operation == null ) {
-      return events;
+    public static final AuditManager getInstance() {
+        if (instance == null) {
+            instance = new AuditManager();
+        }
+        return instance;
     }
-    // Filter out the specified operation only (File open)
+
+    public static final IAuditManager getActive() {
+        return getInstance().getActiveAuditManager();
+    }
+
+    /**
+     * Gets activeAuditManager
+     *
+     * @return value of activeAuditManager
+     */
+    public IAuditManager getActiveAuditManager() {
+        return activeAuditManager;
+    }
+
+    /** @param activeAuditManager The activeAuditManager to set */
+    public void setActiveAuditManager(IAuditManager activeAuditManager) {
+        this.activeAuditManager = activeAuditManager;
+    }
+
+    // Convenience methods...
     //
-    List<AuditEvent> operationEvents = new ArrayList<>();
-    for ( AuditEvent event : events ) {
-      if ( event.getOperation().equalsIgnoreCase( operation ) ) {
+    public static final void registerEvent(String group, String type, String name, String operation)
+            throws HopException {
+        getActive().storeEvent(new AuditEvent(group, type, name, operation, new Date()));
+    }
 
-        if (unique) {
-          if (!names.contains( event.getName() )) {
-            operationEvents.add( event );
-            names.add(event.getName());
-          }
-        } else {
-          operationEvents.add( event );
+    public static final List<AuditEvent> findEvents(
+            String group, String type, String operation, int maxNrEvents, boolean unique)
+            throws HopException {
+        List<AuditEvent> events = getActive().findEvents(group, type, unique);
+        Set<String> names = new HashSet<>();
+
+        if (operation == null) {
+            return events;
         }
-        if ( maxNrEvents > 0 && operationEvents.size() >= maxNrEvents ) {
-          break;
+        // Filter out the specified operation only (File open)
+        //
+        List<AuditEvent> operationEvents = new ArrayList<>();
+        for (AuditEvent event : events) {
+            if (event.getOperation().equalsIgnoreCase(operation)) {
+
+                if (unique) {
+                    if (!names.contains(event.getName())) {
+                        operationEvents.add(event);
+                        names.add(event.getName());
+                    }
+                } else {
+                    operationEvents.add(event);
+                }
+                if (maxNrEvents > 0 && operationEvents.size() >= maxNrEvents) {
+                    break;
+                }
+            }
         }
-      }
+        return operationEvents;
     }
-    return operationEvents;
-  }
 
-  public static final void storeState( ILogChannel log, String group, String type, String name, Map<String, Object> stateProperties ) {
-    AuditState auditState = new AuditState(name, stateProperties);
-    try {
-      getActive().storeState( group, type, auditState );
-    } catch ( Exception e ) {
-      log.logError( "Error writing audit state of type " + type, e );
+    public static final void storeState(
+            ILogChannel log,
+            String group,
+            String type,
+            String name,
+            Map<String, Object> stateProperties) {
+        AuditState auditState = new AuditState(name, stateProperties);
+        try {
+            getActive().storeState(group, type, auditState);
+        } catch (Exception e) {
+            log.logError("Error writing audit state of type " + type, e);
+        }
     }
-  }
 
-  public static final AuditState retrieveState( ILogChannel log, String group, String type, String name ) {
-    try {
-      return getActive().retrieveState( group, type, name );
-    } catch(Exception e) {
-      log.logError( "Error retrieving state of type "+type );
-      return null;
+    public static final AuditState retrieveState(
+            ILogChannel log, String group, String type, String name) {
+        try {
+            return getActive().retrieveState(group, type, name);
+        } catch (Exception e) {
+            log.logError("Error retrieving state of type " + type);
+            return null;
+        }
     }
-  }
 }
--- engine/src/main/java/org/apache/hop/history/AuditState.java
+++ engine/src/main/java/org/apache/hop/history/AuditState.java
@@ -19,78 +19,73 @@
  * limitations under the License.
  *
  ******************************************************************************/
-
 package org.apache.hop.history;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-/**
- * This class allows you to describe the state of objects like loaded files, windows and so on
- */
+/** This class allows you to describe the state of objects like loaded files, windows and so on */
 public class AuditState {
 
-  // The name of the parent (filename, window name, ...)
-  private String name;
+    // The name of the parent (filename, window name, ...)
+    private String name;
 
-  // The various states of the parent properties (width, height, active, zoom, ...)
-  //
-  private Map<String,Object> stateMap;
+    // The various states of the parent properties (width, height, active, zoom, ...)
+    //
+    private Map<String, Object> stateMap;
 
-  public AuditState() {
-    stateMap = new HashMap<>();
-  }
-
-  public AuditState( String name, Map<String, Object> stateMap ) {
-    this.name = name;
-    this.stateMap = stateMap;
-  }
-
-  @Override public boolean equals( Object o ) {
-    if ( this == o ) {
-      return true;
+    public AuditState() {
+        stateMap = new HashMap<>();
     }
-    if ( o == null || getClass() != o.getClass() ) {
-      return false;
+
+    public AuditState(String name, Map<String, Object> stateMap) {
+        this.name = name;
+        this.stateMap = stateMap;
     }
-    AuditState that = (AuditState) o;
-    return  name.equals( that.name );
-  }
 
-  @Override public int hashCode() {
-    return Objects.hash( name );
-  }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        AuditState that = (AuditState) o;
+        return name.equals(that.name);
+    }
 
-  /**
-   * Gets name
-   *
-   * @return value of name
-   */
-  public String getName() {
-    return name;
-  }
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 
-  /**
-   * @param name The name to set
-   */
-  public void setName( String name ) {
-    this.name = name;
-  }
+    /**
+     * Gets name
+     *
+     * @return value of name
+     */
+    public String getName() {
+        return name;
+    }
 
-  /**
-   * Gets stateMap
-   *
-   * @return value of stateMap
-   */
-  public Map<String, Object> getStateMap() {
-    return stateMap;
-  }
+    /** @param name The name to set */
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  /**
-   * @param stateMap The stateMap to set
-   */
-  public void setStateMap( Map<String, Object> stateMap ) {
-    this.stateMap = stateMap;
-  }
+    /**
+     * Gets stateMap
+     *
+     * @return value of stateMap
+     */
+    public Map<String, Object> getStateMap() {
+        return stateMap;
+    }
+
+    /** @param stateMap The stateMap to set */
+    public void setStateMap(Map<String, Object> stateMap) {
+        this.stateMap = stateMap;
+    }
 }
