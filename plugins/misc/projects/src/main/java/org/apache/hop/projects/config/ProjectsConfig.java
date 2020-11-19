fd0ed63263246603ca6656886f5d6b7a137fb34d
Matt Casters <mattcasters@gmail.com>
2020-11-19 09:59:42 +0100

    HOP-2197 : Changing variable in project dialog requires restart

--- plugins/misc/projects/src/main/java/org/apache/hop/projects/config/ProjectsConfig.java
+++ plugins/misc/projects/src/main/java/org/apache/hop/projects/config/ProjectsConfig.java
@@ -1,27 +1,23 @@
-/*! ******************************************************************************
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * Hop : The Hop Orchestration Platform
- *
- * http://www.project-hop.org
- *
- *******************************************************************************
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- ******************************************************************************/
+ */
 
 package org.apache.hop.projects.config;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.hop.projects.environment.LifecycleEnvironment;
 import org.apache.hop.projects.lifecycle.ProjectLifecycle;
 import org.apache.hop.projects.project.ProjectConfig;
@@ -50,18 +46,21 @@ public class ProjectsConfig {
     projectLifecycles = new ArrayList<>();
   }
 
-  public ProjectsConfig( ProjectsConfig config ) {
+  public ProjectsConfig(ProjectsConfig config) {
     this();
     enabled = config.enabled;
     openingLastProjectAtStartup = config.openingLastProjectAtStartup;
-    projectConfigurations = new ArrayList<>( config.projectConfigurations );
-    lifecycleEnvironments = new ArrayList<>( config.lifecycleEnvironments );
-    projectLifecycles = new ArrayList<>( config.projectLifecycles );
+    projectConfigurations = new ArrayList<>(config.projectConfigurations);
+    lifecycleEnvironments = new ArrayList<>(config.lifecycleEnvironments);
+    projectLifecycles = new ArrayList<>(config.projectLifecycles);
   }
 
-  public ProjectConfig findProjectConfig( String projectName ) {
-    for ( ProjectConfig projectConfig : projectConfigurations ) {
-      if ( projectConfig.getProjectName().equalsIgnoreCase( projectName ) ) {
+  public ProjectConfig findProjectConfig(String projectName) {
+    if (StringUtils.isEmpty(projectName)) {
+      return null;
+    }
+    for (ProjectConfig projectConfig : projectConfigurations) {
+      if (projectConfig.getProjectName().equalsIgnoreCase(projectName)) {
         return projectConfig;
       }
     }
@@ -74,31 +73,38 @@ public class ProjectsConfig {
    * @param projectName The name of the environment to look up
    * @return The environments for the project
    */
-  public List<LifecycleEnvironment> findEnvironmentsOfProject( String projectName ) {
+  public List<LifecycleEnvironment> findEnvironmentsOfProject(String projectName) {
     List<LifecycleEnvironment> list = new ArrayList<>();
-    lifecycleEnvironments.stream().forEach( e -> { if (e.getProjectName().equals( projectName )) { list.add(e); } } );
+    lifecycleEnvironments.stream()
+        .forEach(
+            e -> {
+              if (e.getProjectName().equals(projectName)) {
+                list.add(e);
+              }
+            });
     return list;
   }
 
-  public void addProjectConfig( ProjectConfig projectConfig ) {
-    ProjectConfig existing = findProjectConfig( projectConfig.getProjectName() );
-    if ( existing == null ) {
-      projectConfigurations.add( projectConfig );
+  public void addProjectConfig(ProjectConfig projectConfig) {
+    ProjectConfig existing = findProjectConfig(projectConfig.getProjectName());
+    if (existing == null) {
+      projectConfigurations.add(projectConfig);
     } else {
-      existing.setProjectName( projectConfig.getProjectName() );
-      existing.setProjectHome( projectConfig.getProjectHome() );
-      existing.setConfigFilename( projectConfig.getConfigFilename() );
+      existing.setProjectName(projectConfig.getProjectName());
+      existing.setProjectHome(projectConfig.getProjectHome());
+      existing.setConfigFilename(projectConfig.getConfigFilename());
     }
   }
 
-  public int indexOfProjectConfig( String projectName ) {
-    return projectConfigurations.indexOf( new ProjectConfig( projectName, null, null ) ); // Only considers the name
+  public int indexOfProjectConfig(String projectName) {
+    return projectConfigurations.indexOf(
+        new ProjectConfig(projectName, null, null)); // Only considers the name
   }
 
-  public ProjectConfig removeProjectConfig( String projectName ) {
-    int index = indexOfProjectConfig( projectName );
-    if ( index >= 0 ) {
-      return projectConfigurations.remove( index );
+  public ProjectConfig removeProjectConfig(String projectName) {
+    int index = indexOfProjectConfig(projectName);
+    if (index >= 0) {
+      return projectConfigurations.remove(index);
     } else {
       return null;
     }
@@ -106,87 +112,96 @@ public class ProjectsConfig {
 
   public List<String> listProjectConfigNames() {
     List<String> names = new ArrayList<>();
-    projectConfigurations.stream().forEach( config -> names.add( config.getProjectName() ) );
-    Collections.sort( names );
+    projectConfigurations.stream().forEach(config -> names.add(config.getProjectName()));
+    Collections.sort(names);
     return names;
   }
 
-
-  public LifecycleEnvironment findEnvironment( String environmentName ) {
-    for ( LifecycleEnvironment environment : lifecycleEnvironments ) {
-      if ( environment.getName().equals( environmentName ) ) {
+  public LifecycleEnvironment findEnvironment(String environmentName) {
+    if (StringUtils.isEmpty(environmentName)) {
+      return null;
+    }
+    for (LifecycleEnvironment environment : lifecycleEnvironments) {
+      if (environment.getName().equals(environmentName)) {
         return environment;
       }
     }
     return null;
   }
 
-  public void addEnvironment( LifecycleEnvironment environment ) {
-    int index = lifecycleEnvironments.indexOf( environment );
-    if ( index < 0 ) {
-      lifecycleEnvironments.add( environment );
+  public void addEnvironment(LifecycleEnvironment environment) {
+    int index = lifecycleEnvironments.indexOf(environment);
+    if (index < 0) {
+      lifecycleEnvironments.add(environment);
     } else {
-      lifecycleEnvironments.set( index, environment );
+      lifecycleEnvironments.set(index, environment);
     }
   }
 
-  public LifecycleEnvironment removeEnvironment( String environmentName ) {
-    LifecycleEnvironment environment = findEnvironment( environmentName );
-    if ( environment != null ) {
-      lifecycleEnvironments.remove( environment );
+  public LifecycleEnvironment removeEnvironment(String environmentName) {
+    LifecycleEnvironment environment = findEnvironment(environmentName);
+    if (environment != null) {
+      lifecycleEnvironments.remove(environment);
     }
     return environment;
   }
 
   public List<String> listEnvironmentNames() {
     List<String> names = new ArrayList<>();
-    lifecycleEnvironments.stream().forEach( env -> names.add( env.getName() ) );
-    Collections.sort( names );
+    lifecycleEnvironments.stream().forEach(env -> names.add(env.getName()));
+    Collections.sort(names);
     return names;
   }
 
-  public int indexOfEnvironment( String environmentName ) {
-    return lifecycleEnvironments.indexOf( new LifecycleEnvironment( environmentName, null, null, Collections.emptyList() ) ); // Only considers the name
+  public int indexOfEnvironment(String environmentName) {
+    return lifecycleEnvironments.indexOf(
+        new LifecycleEnvironment(
+            environmentName, null, null, Collections.emptyList())); // Only considers the name
   }
 
-
-  public ProjectLifecycle findLifecycle( String lifecycleName ) {
-    for ( ProjectLifecycle lifecycle : projectLifecycles ) {
-      if ( lifecycle.equals( lifecycleName ) ) {
+  public ProjectLifecycle findLifecycle(String lifecycleName) {
+    if (StringUtils.isEmpty(lifecycleName)) {
+      return null;
+    }
+    for (ProjectLifecycle lifecycle : projectLifecycles) {
+      if (lifecycle.equals(lifecycleName)) {
         return lifecycle;
       }
     }
     return null;
   }
 
-  public void addLifecycle( ProjectLifecycle lifecycle ) {
-    int index = projectLifecycles.indexOf( lifecycle );
-    if ( index < 0 ) {
-      projectLifecycles.add( lifecycle );
+  public void addLifecycle(ProjectLifecycle lifecycle) {
+    int index = projectLifecycles.indexOf(lifecycle);
+    if (index < 0) {
+      projectLifecycles.add(lifecycle);
     } else {
-      projectLifecycles.set( index, lifecycle );
+      projectLifecycles.set(index, lifecycle);
     }
   }
 
-  public ProjectLifecycle removeLifecycle( String lifecycleName ) {
-    ProjectLifecycle lifecycle = findLifecycle( lifecycleName );
-    if ( lifecycle != null ) {
-      lifecycleEnvironments.remove( lifecycle );
+  public ProjectLifecycle removeLifecycle(String lifecycleName) {
+    ProjectLifecycle lifecycle = findLifecycle(lifecycleName);
+    if (lifecycle != null) {
+      lifecycleEnvironments.remove(lifecycle);
     }
     return lifecycle;
   }
 
   public List<String> listLifecycleNames() {
     List<String> names = new ArrayList<>();
-    projectLifecycles.stream().forEach( lifecycle -> names.add( lifecycle.getName() ) );
-    Collections.sort( names );
+    projectLifecycles.stream().forEach(lifecycle -> names.add(lifecycle.getName()));
+    Collections.sort(names);
     return names;
   }
 
-  public int indexOfLifecycle( String lifecycleName ) {
-    return projectLifecycles.indexOf( new ProjectLifecycle( lifecycleName, Collections.emptyList(), Collections.emptyList() ) ); // Only considers the name
+  public int indexOfLifecycle(String lifecycleName) {
+    return projectLifecycles.indexOf(
+        new ProjectLifecycle(
+            lifecycleName,
+            Collections.emptyList(),
+            Collections.emptyList())); // Only considers the name
   }
-
 
   /**
    * Gets enabled
@@ -197,10 +212,8 @@ public class ProjectsConfig {
     return enabled;
   }
 
-  /**
-   * @param enabled The enabled to set
-   */
-  public void setEnabled( boolean enabled ) {
+  /** @param enabled The enabled to set */
+  public void setEnabled(boolean enabled) {
     this.enabled = enabled;
   }
 
@@ -213,10 +226,8 @@ public class ProjectsConfig {
     return openingLastProjectAtStartup;
   }
 
-  /**
-   * @param openingLastProjectAtStartup The openingLastProjectAtStartup to set
-   */
-  public void setOpeningLastProjectAtStartup( boolean openingLastProjectAtStartup ) {
+  /** @param openingLastProjectAtStartup The openingLastProjectAtStartup to set */
+  public void setOpeningLastProjectAtStartup(boolean openingLastProjectAtStartup) {
     this.openingLastProjectAtStartup = openingLastProjectAtStartup;
   }
 
@@ -229,10 +240,8 @@ public class ProjectsConfig {
     return projectConfigurations;
   }
 
-  /**
-   * @param projectConfigurations The projectConfigurations to set
-   */
-  public void setProjectConfigurations( List<ProjectConfig> projectConfigurations ) {
+  /** @param projectConfigurations The projectConfigurations to set */
+  public void setProjectConfigurations(List<ProjectConfig> projectConfigurations) {
     this.projectConfigurations = projectConfigurations;
   }
 
@@ -245,10 +254,8 @@ public class ProjectsConfig {
     return lifecycleEnvironments;
   }
 
-  /**
-   * @param lifecycleEnvironments The lifecycleEnvironments to set
-   */
-  public void setLifecycleEnvironments( List<LifecycleEnvironment> lifecycleEnvironments ) {
+  /** @param lifecycleEnvironments The lifecycleEnvironments to set */
+  public void setLifecycleEnvironments(List<LifecycleEnvironment> lifecycleEnvironments) {
     this.lifecycleEnvironments = lifecycleEnvironments;
   }
 }
--- plugins/misc/projects/src/main/java/org/apache/hop/projects/gui/ProjectsGuiPlugin.java
+++ plugins/misc/projects/src/main/java/org/apache/hop/projects/gui/ProjectsGuiPlugin.java
@@ -1,24 +1,19 @@
-/*! ******************************************************************************
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * Hop : The Hop Orchestration Platform
- *
- * http://www.project-hop.org
- *
- *******************************************************************************
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- ******************************************************************************/
+ */
 
 package org.apache.hop.projects.gui;
 
@@ -34,6 +29,7 @@ import org.apache.hop.core.variables.IVariables;
 import org.apache.hop.core.variables.Variables;
 import org.apache.hop.history.AuditEvent;
 import org.apache.hop.history.AuditManager;
+import org.apache.hop.i18n.BaseMessages;
 import org.apache.hop.metadata.api.IHopMetadataProvider;
 import org.apache.hop.metadata.api.IHopMetadataSerializer;
 import org.apache.hop.pipeline.config.PipelineRunConfiguration;
@@ -119,6 +115,23 @@ public class ProjectsGuiPlugin {
 
         refreshProjectsList();
         selectProjectInList( projectName );
+
+        if (projectDialog.isVariablesChanged()) {
+          MessageBox box = new MessageBox( hopGui.getShell(), SWT.ICON_QUESTION | SWT.YES | SWT.NO );
+          box.setText( "Reload project?");
+          box.setMessage( "Do you want to reload this project to apply changed variables?" );
+          int answer = box.open();
+          if ((answer&SWT.YES)!=0) {
+            // Try to stick to the same environment if we have one selected...
+            //
+            LifecycleEnvironment environment = null;
+            Combo environmentsCombo = getEnvironmentsCombo();
+            if (environmentsCombo!=null) {
+              environment = config.findEnvironment( environmentsCombo.getText() );
+            }
+            enableHopGuiProject( projectConfig.getProjectName(), project, environment );
+          }
+        }
       }
     } catch ( Exception e ) {
       new ErrorDialog( hopGui.getShell(), "Error", "Error editing project '" + projectName, e );
