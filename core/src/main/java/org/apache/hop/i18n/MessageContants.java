b635ef3a0f2c49ee7850d53001d04c95d68fbfff
Mahendran Mookkiah <mahendran@apache.org>
2020-11-20 01:06:52 -0500

    Creating Constants for i18n messages
    Signed-off-by: Mahendran Mookkiah <mahendran@apache.org>

---
+++ core/src/main/java/org/apache/hop/i18n/MessageContants.java
@@ -0,0 +1,7 @@
+package org.apache.hop.i18n;
+
+public interface MessageContants {
+
+  public static final String SYSTEM_COMBO_YES = "System.Combo.Yes";
+  public static final String SYSTEM_COMBO_NO = "System.Combo.No";
+}
