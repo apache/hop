f12f95c73ff0fe0839ebb84dcc2ecb0d1760b44c
Matt Casters <mattcasters@gmail.com>
2020-11-24 10:29:36 +0100

    HOP-2221 : Clear custom options in the options dialog doesn't work (#410)

--- core/src/main/java/org/apache/hop/core/Props.java
+++ core/src/main/java/org/apache/hop/core/Props.java
@@ -1,32 +1,31 @@
-/*! ******************************************************************************
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * Hop : The Hop Orchestration Platform
- *
- * Copyright (C) 2002-2018 by Hitachi Vantara : http://www.pentaho.com
- *
- *******************************************************************************
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- ******************************************************************************/
+ */
 
 package org.apache.hop.core;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.hop.core.config.HopConfig;
+import org.apache.hop.core.exception.HopException;
 import org.apache.hop.core.logging.ILogChannel;
 import org.apache.hop.core.logging.LogChannel;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -178,14 +177,16 @@ public class Props implements Cloneable {
     setProperty( STRING_CUSTOM_PARAMETER + parameterName, value );
   }
 
-  public void clearCustomParameters() {
-    Map<String, Object> configMap = HopConfig.getInstance().getConfigMap();
-    for (String key : configMap.keySet() ) {
-      if ( key.startsWith( STRING_CUSTOM_PARAMETER ) ) {
-        // Clear this one
-        configMap.remove( key );
+  public void clearCustomParameters() throws HopException {
+    Map<String, String> configMap = HopConfig.readGuiProperties();
+    Iterator<Map.Entry<String, String>> iterator = configMap.entrySet().iterator();
+    while (iterator.hasNext()) {
+      Map.Entry<String, String> entry = iterator.next();
+      if (entry.getKey().startsWith( STRING_CUSTOM_PARAMETER )) {
+        iterator.remove();
       }
     }
+    HopConfig.getInstance().saveToFile();
   }
 
   public void reset() {
