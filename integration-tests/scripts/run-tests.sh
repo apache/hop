bcc9809f6f68315737486578b88afa9ad518febc
Matt Casters <mattcasters@gmail.com>
2020-12-17 12:01:08 +0100

    HOP-2312 : Running integration tests locally is dangerous

--- integration-tests/scripts/run-tests.sh
+++ integration-tests/scripts/run-tests.sh
@@ -1,25 +1,42 @@
 #!/bin/bash
 
-current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 if [ -z "${HOP_LOCATION}" ]; then
     HOP_LOCATION=/opt/hop
 fi
 
 #set global variables
-spacer="==========================================="
+SPACER="==========================================="
+
+# Set up a temporary folder
+export TMP_FOLDER=/tmp/hop-it-$$
+rm -rf "${TMP_FOLDER}"
+mkdir -p "${TMP_FOLDER}"
 
 #cleanup Temp
-rm -f /tmp/testcases
-rm -rf $current_dir/../surefire-reports
-mkdir -p $current_dir/../surefire-reports/
+export TMP_TESTCASES="${TMP_FOLDER}"/testcases.xml
+rm -f "${TMP_TESTCASES}"
+rm -rf "${CURRENT_DIR}"/../surefire-reports
+mkdir -p "${CURRENT_DIR}"/../surefire-reports/
+
+# Set up auditing and conf folders
+# Start with a new blank slate every time
+# This means it's not needed to delete a project
+#
+export HOP_CONFIG_FOLDER="${TMP_FOLDER}"/config
+rm -rf "${HOP_CONFIG_FOLDER}"
+mkdir -p "${HOP_CONFIG_FOLDER}"
+export HOP_AUDIT_FOLDER="${TMP_FOLDER}"/audit
+rm -rf "${HOP_AUDIT_FOLDER}"
+mkdir -p "${HOP_AUDIT_FOLDER}"
 
 #Loop over project folders
-for d in $current_dir/../*/ ; do
+for d in "${CURRENT_DIR}"/../*/ ; do
     #cleanup project testcases
-    rm -f /tmp/testcases
+    rm -f "${TMP_TESTCASES}"
 
-    if [[ "$d" != *"scripts/" ]] && [[ "$d" != *"surefire-reports/" ]]; then
+    if [[ "$d" != *"scripts/" ]] && [[ "$d" != *"surefire-reports/" ]] ; then
 
         #set test variables
         start_time=$SECONDS
@@ -28,22 +45,20 @@ for d in $current_dir/../*/ ; do
         skipped_counter=0
         failures_counter=0
 
-        project_name=$(basename $d)
+        PROJECT_NAME=$(basename $d)
 
-        echo $spacer
-        echo "Starting Tests in project: $project_name"
-        echo $spacer
+        echo ${SPACER}
+        echo "Starting Tests in project: ${PROJECT_NAME}"
+        echo ${SPACER}
 
-        #Increment timer and set test start time
+        # Increment timer and set test start time
         test_counter=$((test_counter+1))
-        
-        #Delete project first
-        $HOP_LOCATION/hop-conf.sh -pd -p $project_name
 
-        #Create New Project
-        $HOP_LOCATION/hop-conf.sh -pc -p $project_name -ph "$(readlink -f $d)"
+        # Create New Project
+        $HOP_LOCATION/hop-conf.sh -pc -p ${PROJECT_NAME} -ph "$(readlink -f $d)"
 
-        #Find main hwf files TODO: add hpl support when result is returned correctly
+        # Find main hwf files 
+        # TODO: add hpl support when result is returned correctly
         for f in $d/main*.hwf ; do
 
             #cleanup temp files
@@ -58,15 +73,15 @@ for d in $current_dir/../*/ ; do
             test_name=${test_name//'.hwf'/}
 
             #Starting Test
-            echo $spacer
+            echo ${SPACER}
             echo "Starting Test: $test_name"
-            echo $spacer
+            echo ${SPACER}
 
             #Start time test
             start_time_test=$SECONDS
 
             #Run Test
-            $HOP_LOCATION/hop-run.sh -j $project_name -r "local" -f $hop_file > >(tee /tmp/test_output) 2> >(tee /tmp/test_output_err >&1)
+            $HOP_LOCATION/hop-run.sh -j ${PROJECT_NAME} -r "local" -f $hop_file > >(tee /tmp/test_output) 2> >(tee /tmp/test_output_err >&1)
 
             #Capture exit code
             exit_code=${PIPESTATUS[0]}
@@ -74,40 +89,40 @@ for d in $current_dir/../*/ ; do
             #Test time duration
             test_duration=$(( SECONDS - start_time_test ))
 
-            if (( $exit_code >= 1 )) ; 
+            if (( $exit_code >= 1 )) ;
             then
                 errors_counter=$((errors_counter+1))
                 failures_counter=$((failures_counter+1))
                 #Create surefire xml failure
-                echo "<testcase name=\"$test_name\" time=\"$test_duration\">" >> /tmp/testcases
-                echo "<failure type=\"$test_name\"></failure>" >> /tmp/testcases
-                echo "<system-out>" >> /tmp/testcases
-                echo "<![CDATA["  >> /tmp/testcases
-                cat /tmp/test_output >> /tmp/testcases
-                echo "]]>"  >> /tmp/testcases
-                echo "</system-out>" >> /tmp/testcases
-                echo "<system-err>" >> /tmp/testcases
-                echo "<![CDATA["  >> /tmp/testcases
-                cat /tmp/test_output_err >> /tmp/testcases
-                echo "]]>"  >> /tmp/testcases
-                echo "</system-err>" >> /tmp/testcases
-                echo "</testcase>" >> /tmp/testcases
+                echo "<testcase name=\"$test_name\" time=\"$test_duration\">" >> ${TMP_TESTCASES}
+                echo "<failure type=\"$test_name\"></failure>" >> ${TMP_TESTCASES}
+                echo "<system-out>" >> ${TMP_TESTCASES}
+                echo "<![CDATA["  >> ${TMP_TESTCASES}
+                cat /tmp/test_output >> ${TMP_TESTCASES}
+                echo "]]>"  >> ${TMP_TESTCASES}
+                echo "</system-out>" >> ${TMP_TESTCASES}
+                echo "<system-err>" >> ${TMP_TESTCASES}
+                echo "<![CDATA["  >> ${TMP_TESTCASES}
+                cat /tmp/test_output_err >> ${TMP_TESTCASES}
+                echo "]]>"  >> ${TMP_TESTCASES}
+                echo "</system-err>" >> ${TMP_TESTCASES}
+                echo "</testcase>" >> ${TMP_TESTCASES}
 
             else
                 #Create surefire xml success
-                echo "<testcase name=\"$test_name\" time=\"$test_duration\">" >> /tmp/testcases
-                echo "<system-out>" >> /tmp/testcases
-                echo "<![CDATA["  >> /tmp/testcases
-                cat /tmp/test_output >> /tmp/testcases
-                echo "]]>"  >> /tmp/testcases
-                echo "</system-out>" >> /tmp/testcases
-                echo "</testcase>" >> /tmp/testcases
+                echo "<testcase name=\"$test_name\" time=\"$test_duration\">" >> ${TMP_TESTCASES}
+                echo "<system-out>" >> ${TMP_TESTCASES}
+                echo "<![CDATA["  >> ${TMP_TESTCASES}
+                cat /tmp/test_output >> ${TMP_TESTCASES}
+                echo "]]>"  >> ${TMP_TESTCASES}
+                echo "</system-out>" >> ${TMP_TESTCASES}
+                echo "</testcase>" >> ${TMP_TESTCASES}
             fi
 
             #Print results to console
-            echo $spacer
+            echo ${SPACER}
             echo "Test Result"
-            echo $spacer
+            echo ${SPACER}
             echo "Test duration: $test_duration"
             echo "Test Exit Code: $exit_code"
 
@@ -116,19 +131,25 @@ for d in $current_dir/../*/ ; do
         total_duration=$(( SECONDS - start_time ))
 
         #Print End results
-        echo $spacer
+        echo ${SPACER}
         echo "Final Report"
-        echo $spacer
+        echo ${SPACER}
         echo "Number of Tests: $test_counter"
         echo "Total errors: $errors_counter"
         echo "Total faliures: $failures_counter"
         echo "Total duration: $total_duration"
 
         #create final report
-        echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" > $current_dir/../surefire-reports/surefile_$project_name.xml
-        echo "<testsuite xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"https://maven.apache.org/surefire/maven-surefire-plugin/xsd/surefire-test-report-3.0.xsd\" version=\"3.0\" name=\"$project_name\" time=\"$total_duration\" tests=\"$test_counter\" errors=\"$errors_counter\" skipped=\"$skipped_counter\" failures=\"$failures_counter\">" >> $current_dir/../surefire-reports/surefile_$project_name.xml
-        cat /tmp/testcases >> $current_dir/../surefire-reports/surefile_$project_name.xml
-        echo "</testsuite>" >> $current_dir/../surefire-reports/surefile_$project_name.xml
+        echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" > "${CURRENT_DIR}"/../surefire-reports/surefile_${PROJECT_NAME}.xml
+        echo "<testsuite xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"https://maven.apache.org/surefire/maven-surefire-plugin/xsd/surefire-test-report-3.0.xsd\" version=\"3.0\" name=\"${PROJECT_NAME}\" time=\"$total_duration\" tests=\"$test_counter\" errors=\"$errors_counter\" skipped=\"$skipped_counter\" failures=\"$failures_counter\">" >> "${CURRENT_DIR}"/../surefire-reports/surefile_${PROJECT_NAME}.xml
+        cat ${TMP_TESTCASES} >> "${CURRENT_DIR}"/../surefire-reports/surefile_${PROJECT_NAME}.xml
+        echo "</testsuite>" >> "${CURRENT_DIR}"/../surefire-reports/surefile_${PROJECT_NAME}.xml
 
     fi
 done
+
+# Cleanup config and audit folders
+#
+rm -rf "${HOP_CONFIG_FOLDER}"
+rm -rf "${HOP_AUDIT_FOLDER}"
+rm -rf "${TMP_FOLDER}"
