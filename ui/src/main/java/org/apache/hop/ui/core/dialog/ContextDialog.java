61c8656c44f87aa1acd265e0ce2ef5ae6d2164b7
Hiromu Hota <hiromu.hota@gmail.com>
2020-12-27 20:04:28 -0800

    No need to redraw Canvas on selecting verticalBar

--- ui/src/main/java/org/apache/hop/ui/core/dialog/ContextDialog.java
+++ ui/src/main/java/org/apache/hop/ui/core/dialog/ContextDialog.java
@@ -507,12 +507,6 @@ public class ContextDialog extends Dialog {
         selectItem( item, false );
       }
     } );
-    wCanvas.getVerticalBar().addSelectionListener( new SelectionAdapter() {
-      @Override
-      public void widgetSelected( SelectionEvent e ) {
-        wCanvas.redraw();
-      }
-    } );
     wCanvas.addKeyListener( keyAdapter );
 
     // Layout all the widgets in the shell.
