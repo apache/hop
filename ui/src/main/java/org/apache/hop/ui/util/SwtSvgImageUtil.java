6225f364a9047c1d53fd40b8dd491c45be82c3b2
Matt Casters <mattcasters@gmail.com>
2021-02-18 12:12:53 +0100

    HOP-2375 : Various color and drawing corrections for dark mode

--- ui/src/main/java/org/apache/hop/ui/util/SwtSvgImageUtil.java
+++ ui/src/main/java/org/apache/hop/ui/util/SwtSvgImageUtil.java
@@ -300,14 +300,6 @@ public class SwtSvgImageUtil {
     } else {
       // svg image - need to convert to bitmap
       try {
-
-/*
-        if (filename.endsWith( "/pipeline.svg" )) {
-          System.out.println("Filename: "+filename);
-          Thread.sleep( 10000 );
-        }
-*/
-
         return new SwtUniversalImageSvg( SvgSupport.loadSvgImage( in ) );
       } catch ( Exception ex ) {
         throw new RuntimeException( ex );
