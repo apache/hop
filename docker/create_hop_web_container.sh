b2782ff0dbf6dcb3fa6143038ef8373679affc16
Hans Van Akelyen <hans.van.akelyen@gmail.com>
2021-02-24 20:22:37 +0100

    HOP-2529: add cleanup to script

--- docker/create_hop_web_container.sh
+++ docker/create_hop_web_container.sh
@@ -18,9 +18,16 @@
 #
 #
 
+#set working dir to current location
 cd "${0%/*}"
 
+#unzip files for docker image
 unzip ../assemblies/web/target/hop.war -d ../assemblies/web/target/webapp
 unzip ../assemblies/plugins/dist/target/hop-assemblies-*.zip -d ../assemblies/plugins/dist/target/
 
+#build docker image
 docker build ../ -f Dockerfile.web -t hop-web
+
+#cleanup
+rm -rf ../assemblies/web/target/webapp
+rm -rf ../assemblies/plugins/dist/target/plugins
