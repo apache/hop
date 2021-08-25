4b8f9754312c5420ff4317bfef5412328c00e0a2
Hans Van Akelyen <hans.van.akelyen@gmail.com>
2021-08-25 23:07:51 +0200

    HOP-3187: fix missing shell in VCS.java

--- plugins/misc/git/src/main/java/org/apache/hop/git/model/VCS.java
+++ plugins/misc/git/src/main/java/org/apache/hop/git/model/VCS.java
@@ -15,34 +15,17 @@
  * limitations under the License.
  */
 
-/*
- * Hop : The Hop Orchestration Platform
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.apache.hop.git.model;
 
 import com.google.common.annotations.VisibleForTesting;
 import org.apache.hop.ui.core.dialog.EnterStringDialog;
-import org.apache.hop.ui.hopgui.perspective.HopPerspectivePlugin;
+import org.apache.hop.ui.hopgui.HopGui;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.MessageBox;
-import org.eclipse.swt.widgets.Shell;
 
 public abstract class VCS {
 
-  protected static final Class<?> PKG = HopPerspectivePlugin.class; // For Translator
+  protected static final Class<?> PKG = UIGit.class; // For Translator
 
   public static final String WORKINGTREE = "WORKINGTREE";
   public static final String INDEX = "INDEX";
@@ -50,12 +33,11 @@ public abstract class VCS {
   public static final String TYPE_BRANCH = "branch";
   public static final String TYPE_COMMIT = "commit";
 
-  protected Shell shell;
   protected String directory;
 
   @VisibleForTesting
   void showMessageBox(String title, String message) {
-    MessageBox messageBox = new MessageBox(shell, SWT.OK);
+    MessageBox messageBox = new MessageBox(HopGui.getInstance().getShell(), SWT.OK);
     messageBox.setText(title);
     messageBox.setMessage(message == null ? "" : message);
     messageBox.open();
@@ -68,14 +50,16 @@ public abstract class VCS {
    */
   protected boolean promptUsernamePassword() {
     EnterStringDialog userDialog =
-        new EnterStringDialog(shell, "", "Username?", "Enter the git username to use");
+        new EnterStringDialog(
+            HopGui.getInstance().getShell(), "", "Username?", "Enter the git username to use");
     String username = userDialog.open();
     if (username == null) {
       return false;
     }
 
     EnterStringDialog passDialog =
-        new EnterStringDialog(shell, "", "Password?", "Enter the git password to use");
+        new EnterStringDialog(
+            HopGui.getInstance().getShell(), "", "Password?", "Enter the git password to use");
     passDialog.setEchoChar('*');
     String password = passDialog.open();
     if (password == null) {
