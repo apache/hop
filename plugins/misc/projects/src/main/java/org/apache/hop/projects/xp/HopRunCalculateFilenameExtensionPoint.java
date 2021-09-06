bd7f14421b3f91699aa179ea1b83b04dde87f534
Matt Casters <mattcasters@gmail.com>
2021-09-06 15:54:46 +0200

    HOP-3252 : Executing with hop-run is too slow

--- plugins/misc/projects/src/main/java/org/apache/hop/projects/xp/HopRunCalculateFilenameExtensionPoint.java
+++ plugins/misc/projects/src/main/java/org/apache/hop/projects/xp/HopRunCalculateFilenameExtensionPoint.java
@@ -6,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -37,7 +37,6 @@ public class HopRunCalculateFilenameExtensionPoint implements IExtensionPoint<Ho
       throws HopException {
 
     try {
-      Thread.sleep(10000);
       FileObject fileObject = HopVfs.getFileObject(hopRun.getRealFilename());
       if (!fileObject.exists()) {
         // Try to prepend with ${PROJECT_HOME}
