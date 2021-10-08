ad2f4d1bde570c4b1c5858723b953eab86ccc9cf
Matt Casters <mattcasters@gmail.com>
2021-10-08 09:18:35 +0200

    HOP-3372 : Transform Get Variables shouldn't guess field lengths

--- plugins/transforms/getvariable/src/main/java/org/apache/hop/pipeline/transforms/getvariable/GetVariableMeta.java
+++ plugins/transforms/getvariable/src/main/java/org/apache/hop/pipeline/transforms/getvariable/GetVariableMeta.java
@@ -6,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -157,19 +157,6 @@ public class GetVariableMeta extends BaseTransformMeta
       IVariables variables,
       IHopMetadataProvider metadataProvider)
       throws HopTransformException {
-    // Determine the maximum length...
-    //
-    int length = -1;
-    for (int i = 0; i < fieldDefinitions.length; i++) {
-      String variableString = fieldDefinitions[i].getVariableString();
-      if (variableString != null) {
-        String string = variables.resolve(variableString);
-        if (string.length() > length) {
-          length = string.length();
-        }
-      }
-    }
-
     IRowMeta row = new RowMeta();
     for (int i = 0; i < fieldDefinitions.length; i++) {
       IValueMeta v;
@@ -181,11 +168,7 @@ public class GetVariableMeta extends BaseTransformMeta
         throw new HopTransformException(e);
       }
       int fieldLength = fieldDefinitions[i].getFieldLength();
-      if (fieldLength < 0) {
-        v.setLength(length);
-      } else {
-        v.setLength(fieldLength);
-      }
+      v.setLength(fieldLength);
       int fieldPrecision = fieldDefinitions[i].getFieldPrecision();
       if (fieldPrecision >= 0) {
         v.setPrecision(fieldPrecision);
