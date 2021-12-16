3d70b2285f84a251883687c06e82a1ce825cbdcc
Matt Casters <mattcasters@gmail.com>
2021-12-16 09:59:35 +0100

    HOP-3533 : Ability to set the METADATA_FOLDER in long-lived hop-server HOP-3548 : Support projects and environments in the long lived docker container HOP-3549 : Support SSL configurations in long lived container HOP-3552 : Add docker options for the server logging options

--- docker/resources/load-and-execute.sh
+++ docker/resources/load-and-execute.sh
@@ -15,7 +15,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-#
 
 set -Eeuo pipefail
 
@@ -34,7 +33,6 @@ log() {
 write_server_config() {
     HOP_SERVER_USER=${HOP_SERVER_USER:-cluster}
     HOP_SERVER_PASS=${HOP_SERVER_PASS:-cluster}
-    HOP_SERVER_MASTER=${HOP_SERVER_MASTER:-Y}
     HOP_SERVER_HOSTNAME=${HOP_SERVER_HOSTNAME:-0.0.0.0}
 
     HOP_SERVER_XML=/tmp/hop-server.xml
@@ -48,9 +46,53 @@ write_server_config() {
     echo "    <port>${HOP_SERVER_PORT}</port>" >> ${HOP_SERVER_XML}
     echo "    <username>${HOP_SERVER_USER}</username>" >> ${HOP_SERVER_XML}
     echo "    <password>${HOP_SERVER_PASS}</password>" >> ${HOP_SERVER_XML}
-    echo "  </hop-server>" >> ${HOP_SERVER_XML}
-    echo "</hop-server-config>" >> ${HOP_SERVER_XML}
 
+    # If an SSL configuration is needed we need to include it here
+    #
+    if [ ! -z "${HOP_SERVER_KEYSTORE}"]
+    then
+      log "Configuring SSL with key store file: ${HOP_SERVER_KEYSTORE}"
+      echo "    <sslConfig>" >> ${HOP_SERVER_XML}
+      echo "      <keyStore>${HOP_SERVER_KEYSTORE}</keyStore>" >> ${HOP_SERVER_XML}
+      echo "      <keyStorePassword>${HOP_SERVER_KEYSTORE_PASSWORD}</keyStorePassword>" >> ${HOP_SERVER_XML}
+      if [ ! -z "${HOP_SERVER_KEY_PASSWORD}"]
+      then
+        echo "      <keyPassword>${HOP_SERVER_KEY_PASSWORD}</keyPassword>" >> ${HOP_SERVER_XML}
+      fi
+      echo "    </sslConfig>" >> ${HOP_SERVER_XML}
+    fi
+    echo "  </hop-server>" >> ${HOP_SERVER_XML}
+
+    # If the metadata folder is set, include it in the configuration
+    #
+    if [ ! -z "${HOP_SERVER_METADATA_FOLDER}" ]
+    then
+      log "The server metadata is in folder: ${HOP_SERVER_METADATA_FOLDER}"
+      echo "  <metadata_folder>${HOP_SERVER_METADATA_FOLDER}</metadata_folder>" >> ${HOP_SERVER_XML}
+    fi
+    # The time (in minutes) it takes for a log line to be cleaned up in memory.
+    #
+    if [ ! -z "${HOP_SERVER_MAX_LOG_LINES}" ]
+    then
+      log "The maximum amount of log lines kept is: ${HOP_SERVER_MAX_LOG_LINES}"
+      echo "  <max_log_lines>${HOP_SERVER_MAX_LOG_LINES}</max_log_lines>" >> ${HOP_SERVER_XML}
+    fi
+    # The time (in minutes) that log lines are kept in memory by the server
+    #
+    if [ ! -z "${HOP_SERVER_MAX_LOG_TIMEOUT}" ]
+    then
+      log "Log lines timeout (in minutes) is: ${HOP_SERVER_MAX_LOG_TIMEOUT}"
+      echo "  <max_log_timeout_minutes>${HOP_SERVER_MAX_LOG_TIMEOUT}</max_log_timeout_minutes>" >> ${HOP_SERVER_XML}
+    fi
+    # The time (in minutes) it takes for a pipeline or workflow execution to be removed from the server status.
+    #
+    if [ ! -z "${HOP_SERVER_MAX_OBJECT_TIMEOUT}" ]
+    then
+      log "Object timeout (in minutes) is: ${HOP_SERVER_MAX_OBJECT_TIMEOUT}"
+      echo "  <object_timeout_minutes>${HOP_SERVER_MAX_OBJECT_TIMEOUT}</object_timeout_minutes>" >> ${HOP_SERVER_XML}
+    fi
+
+    echo "</hop-server-config>" >> ${HOP_SERVER_XML}
 }
 
 # retrieve files from volume
@@ -59,45 +101,117 @@ write_server_config() {
 # allow customisation
 # e.g. to fetch hop project files from S3 or github
 if test -f "${HOP_CUSTOM_ENTRYPOINT_EXTENSION_SHELL_FILE_PATH}"; then
-  echo "Sourcing custom entry point extension: ${HOP_CUSTOM_ENTRYPOINT_EXTENSION_SHELL_FILE_PATH}"
+  log "Sourcing custom entry point extension: ${HOP_CUSTOM_ENTRYPOINT_EXTENSION_SHELL_FILE_PATH}"
   source ${HOP_CUSTOM_ENTRYPOINT_EXTENSION_SHELL_FILE_PATH}
 fi
 
-if [ -z "${HOP_FILE_PATH}" ]
+# The common execution options for short and long lived containers
+# The default log level is Basic
+#
+HOP_EXEC_OPTIONS="--level=${HOP_LOG_LEVEL}"
+
+# For backward compatibility we'll still understand the HOP_PROJECT_DIRECTORY variable
+#
+if [ -z "${HOP_PROJECT_FOLDER}" ]
 then
+  if [ ! -z "${HOP_PROJECT_DIRECTORY}" ]
+  then
+    log "Using deprecated option ${HOP_PROJECT_DIRECTORY} for option HOP_PROJECT_FOLDER"
+    HOP_PROJECT_FOLDER="${HOP_PROJECT_DIRECTORY}"
+  fi
+fi
 
-    write_server_config
-    log "Starting a hop-server on port "${HOP_SERVER_PORT}
-    ${DEPLOYMENT_PATH}/hop/hop-server.sh /tmp/hop-server.xml
+# If we need to set system properties
+# By default this is not set
+#
+if [ ! -z "${HOP_SYSTEM_PROPERTIES}" ]
+then
+  log "Setting system properties at runtime: ${HOP_SYSTEM_PROPERTIES}"
+  HOP_EXEC_OPTIONS="${HOP_EXEC_OPTIONS} --system-properties=${HOP_SYSTEM_PROPERTIES}"
+fi
 
-else
+# If a project name is defined we assume that we want to create it in the container
+#
+if [ ! -z "${HOP_PROJECT_NAME}" ]
+then
+  # We need the project folder to be set...
+  #
+  if [ -z "${HOP_PROJECT_FOLDER}" ]
+  then
+    log "Error: please set variable HOP_PROJECT_FOLDER to create project ${HOP_PROJECT_NAME}"
+    exit 9;
+  else
+    log "The project folder is set to: ${HOP_PROJECT_FOLDER}"
+  fi
 
-  log "Registering project config with Hop"
-  log "${DEPLOYMENT_PATH}/hop/hop-conf.sh --project=${HOP_PROJECT_NAME} --project-create --project-home='${HOP_PROJECT_DIRECTORY}' --project-config-file='${HOP_PROJECT_CONFIG_FILE_NAME}'"
+  # The project folder should exist
+  #
+  if [ ! -d "${HOP_PROJECT_FOLDER}" ]
+  then
+    log "Error: the folder specified in variable HOP_PROJECT_FOLDER does not exist: ${HOP_PROJECT_FOLDER}"
+    exit 9;
+  else
+    log "The specified project folder exists"
+  fi
+
+  log "Registering project ${HOP_PROJECT_NAME} in the Hop configuration"
+  log "${DEPLOYMENT_PATH}/hop/hop-conf.sh --project=${HOP_PROJECT_NAME} --project-create --project-home='${HOP_PROJECT_FOLDER}' --project-config-file='${HOP_PROJECT_CONFIG_FILE_NAME}'"
 
   ${DEPLOYMENT_PATH}/hop/hop-conf.sh \
   --project=${HOP_PROJECT_NAME} \
   --project-create \
-  --project-home="${HOP_PROJECT_DIRECTORY}" \
+  --project-home="${HOP_PROJECT_FOLDER}" \
   --project-config-file="${HOP_PROJECT_CONFIG_FILE_NAME}"
 
-  log "Registering environment config with Hop"
-  log "${DEPLOYMENT_PATH}/hop/hop-conf.sh --environment-create --environment=${HOP_ENVIRONMENT_NAME} --environment-project=${HOP_PROJECT_NAME} --environment-config-files='${HOP_ENVIRONMENT_CONFIG_FILE_NAME_PATHS}'"
+  HOP_EXEC_OPTIONS="${HOP_EXEC_OPTIONS} --project=${HOP_PROJECT_NAME}"
 
-  ${DEPLOYMENT_PATH}/hop/hop-conf.sh \
-  --environment=${HOP_ENVIRONMENT_NAME} \
-  --environment-create \
-  --environment-project=${HOP_PROJECT_NAME} \
-  --environment-config-files="${HOP_ENVIRONMENT_CONFIG_FILE_NAME_PATHS}"
+  # If we also have a the environment we want to create that as well...
+  #
+  if [ ! -z "${HOP_ENVIRONMENT_NAME}" ]
+  then
+    log "Registering environment config with Hop"
+    log "${DEPLOYMENT_PATH}/hop/hop-conf.sh --environment-create --environment=${HOP_ENVIRONMENT_NAME} --environment-project=${HOP_PROJECT_NAME} --environment-config-files='${HOP_ENVIRONMENT_CONFIG_FILE_NAME_PATHS}'"
+
+    ${DEPLOYMENT_PATH}/hop/hop-conf.sh \
+    --environment=${HOP_ENVIRONMENT_NAME} \
+    --environment-create \
+    --environment-project=${HOP_PROJECT_NAME} \
+    --environment-purpose="Apache Hop docker container" \
+    --environment-config-files="${HOP_ENVIRONMENT_CONFIG_FILE_NAME_PATHS}"
+
+    HOP_EXEC_OPTIONS="${HOP_EXEC_OPTIONS} --environment=${HOP_ENVIRONMENT_NAME}"
+  else
+    log "Not creating an environment in the container"
+  fi
+
+else
+  log "Not creating a project or environment in the container"
+fi
+
+
+if [ -z "${HOP_FILE_PATH}" ]
+then
+    write_server_config
+    log "Starting a hop-server on port "${HOP_SERVER_PORT}
+    ${DEPLOYMENT_PATH}/hop/hop-server.sh \
+      ${HOP_EXEC_OPTIONS} \
+      /tmp/hop-server.xml \
+      2>&1 | tee ${HOP_LOG_PATH}
+
+else
+
+  if [ -z ${HOP_RUN_CONFIG} ]
+  then
+    log "Please specify which run configuration you want to use to execute with variable HOP_RUN_CONFIG"
+    exit 9;
+  fi
 
   log "Running a single hop workflow / pipeline (${HOP_FILE_PATH})"
   ${DEPLOYMENT_PATH}/hop/hop-run.sh \
     --file=${HOP_FILE_PATH} \
-    --project=${HOP_PROJECT_NAME} \
-    --environment=${HOP_ENVIRONMENT_NAME} \
     --runconfig=${HOP_RUN_CONFIG} \
-    --level=${HOP_LOG_LEVEL} \
     --parameters=${HOP_RUN_PARAMETERS} \
+    ${HOP_EXEC_OPTIONS} \
     2>&1 | tee ${HOP_LOG_PATH}
 fi
   
