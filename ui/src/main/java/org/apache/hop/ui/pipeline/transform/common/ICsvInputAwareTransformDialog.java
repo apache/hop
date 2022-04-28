f7a78789277604da749e98c204032d46771187bf
Hans Van Akelyen <hans.van.akelyen@gmail.com>
2022-04-28 11:49:09 +0200

    [SONAR] Remove unused imports

--- ui/src/main/java/org/apache/hop/ui/pipeline/transform/common/ICsvInputAwareTransformDialog.java
+++ ui/src/main/java/org/apache/hop/ui/pipeline/transform/common/ICsvInputAwareTransformDialog.java
@@ -32,7 +32,6 @@ import org.apache.hop.pipeline.transforms.common.ICsvInputAwareMeta;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.text.DecimalFormat;
-import java.util.Locale;
 
 /**
  * A common interface for all transform dialogs aware of the csv input format, such as CSV Input and
