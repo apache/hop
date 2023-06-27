26a9b400630871b4dac2a8800245fc4af6715552
Bart Maertens <bart.maertens@know.bi>
2023-06-27 20:00:23 +0200

    initial commit for google sheets and google analytics 4 plugins #3034 #2657

---
+++ plugins/tech/google/src/main/java/org/apache/hop/pipeline/transforms/googleanalytics/GoogleAnalyticsData.java
@@ -0,0 +1,10 @@
+package org.apache.hop.pipeline.transforms.googleanalytics;
+
+import org.apache.hop.core.row.IRowMeta;
+import org.apache.hop.pipeline.transform.BaseTransformData;
+import org.apache.hop.pipeline.transform.ITransformData;
+
+public class GoogleAnalyticsData extends BaseTransformData implements ITransformData{
+
+    public IRowMeta outputMeta;
+}
