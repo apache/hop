8edecf20a1e4c75da967b7fc65d2e38848f8b98d
Hans Van Akelyen <hans.van.akelyen@know.bi>
2023-10-08 22:15:48 +0200

    add environment for testing

--- docker/integration-tests/spark/build.sh
+++ docker/integration-tests/spark/build.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
-
-#
+#!/usr/bin/env bash
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
 # this work for additional information regarding copyright ownership.
@@ -8,29 +6,22 @@
 # (the "License"); you may not use this file except in compliance with
 # the License.  You may obtain a copy of the License at
 #
-#    http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-export SPARK_MASTER_HOST=${SPARK_MASTER_HOST:-`hostname`}
-
-export SPARK_HOME=/spark
-
-. "/spark/sbin/spark-config.sh"
-
-. "/spark/bin/load-spark-env.sh"
-
-mkdir -p $SPARK_MASTER_LOG
-
-ln -sf /dev/stdout $SPARK_MASTER_LOG/spark-master.out
-
-# # start history server and thrift server 
-# /spark/sbin/start-thriftserver.sh
-
-cd /spark/bin && /spark/sbin/../bin/spark-class org.apache.spark.deploy.master.Master \
-    --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG/spark-master.out
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+set -e
+mkdir -p build
+if [ ! -d "$DIR/build/apache-rat-0.15" ]; then
+	curl -LSs https://dlcdn.apache.org/creadur/apache-rat-0.15/apache-rat-0.15-bin.tar.gz -o "$DIR/build/apache-rat.tar.gz"
+	cd $DIR/build
+	tar zvxf apache-rat.tar.gz
+	cd -
+fi
+java -jar $DIR/build/apache-rat-0.15/apache-rat-0.15.jar $DIR -e public -e apache-rat-0.15 -e .git -e .gitignore
+docker build -t apache/hadoop:3 .
