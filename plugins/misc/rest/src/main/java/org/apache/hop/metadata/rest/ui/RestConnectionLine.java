c82f95fb01f814485f55e4e6b179a279882fdcf7
Bart Maertens <bart.maertens@know.bi>
2024-10-01 13:54:40 +0200

    initial version rest metadata type, first rest transform changes. #4369

---
+++ plugins/misc/rest/src/main/java/org/apache/hop/metadata/rest/ui/RestConnectionLine.java
@@ -0,0 +1,12 @@
+package org.apache.hop.metadata.rest.ui;
+
+import org.eclipse.swt.widgets.Composite;
+
+public class RestConnectionLine extends Composite {
+
+    public RestConnectionLine(Composite parent, int style) {
+        super(parent, style);
+
+
+    }
+}
