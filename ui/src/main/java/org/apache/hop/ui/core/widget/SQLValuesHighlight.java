dd709e6625a65ea8555fe79f2df10c42a40e51d8
Sergio De Lorenzis <sergio@graphaware.com>
2024-10-09 23:44:07 +0200

    Able to query metadata from TableInputDialog

--- ui/src/main/java/org/apache/hop/ui/core/widget/SQLValuesHighlight.java
+++ ui/src/main/java/org/apache/hop/ui/core/widget/SQLValuesHighlight.java
@@ -669,7 +669,7 @@ public class SQLValuesHighlight implements LineStyleListener {
     /** Initialize the lookup table. */
     void initialize() {
       fgKeys = new Hashtable<String, Integer>();
-      Integer k = new Integer(KEY);
+      Integer k = Integer.valueOf(KEY);
       for (int i = 0; i < fgKeywords.length; i++) {
         fgKeys.put(fgKeywords[i], k);
       }
@@ -685,7 +685,7 @@ public class SQLValuesHighlight implements LineStyleListener {
 
     public void initializeSQLFunctions() {
       kfKeys = new Hashtable<String, Integer>();
-      Integer k = new Integer(FUNCTIONS);
+      Integer k = Integer.valueOf(FUNCTIONS);
       for (int i = 0; i < kfKeywords.length; i++) {
         kfKeys.put(kfKeywords[i], k);
       }
