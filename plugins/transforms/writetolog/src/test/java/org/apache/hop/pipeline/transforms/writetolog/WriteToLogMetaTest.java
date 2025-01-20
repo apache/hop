593aa0a42420ca89f25ebf1ec7c640c06f07cf21
Nicolas Adment <39568358+nadment@users.noreply.github.com>
2025-01-20 22:19:45 +0100

    Move test package

--- plugins/transforms/writetolog/src/test/java/org/apache/hop/pipeline/transforms/writetolog/WriteToLogMetaTest.java
+++ plugins/transforms/writetolog/src/test/java/org/apache/hop/pipeline/transforms/writetolog/WriteToLogMetaTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.pipeline.transforms.writetolog;
+package org.apache.hop.pipeline.transforms.writetolog;
 
 import java.util.EnumSet;
 import java.util.HashMap;
