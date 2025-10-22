f030af080d20c2bed3c4c1ca8db33c4950003bb5
Hans Van Akelyen <hans.van.akelyen@gmail.com>
2025-10-22 14:06:34 +0200

    multimerge dialog breaking when hop/transforms are removed, fixes #5868

--- plugins/transforms/multimerge/src/main/java/org/apache/hop/pipeline/transforms/multimerge/MultiMergeJoinDialog.java
+++ plugins/transforms/multimerge/src/main/java/org/apache/hop/pipeline/transforms/multimerge/MultiMergeJoinDialog.java
@@ -88,11 +88,7 @@ public class MultiMergeJoinDialog extends BaseTransformDialog {
   }
 
   private String[] getInputTransformNames() {
-    String[] inputTransformNames = joinMeta.getInputTransforms();
     ArrayList<String> nameList = new ArrayList<>();
-    if (inputTransformNames != null) {
-      Collections.addAll(nameList, inputTransformNames);
-    }
 
     String[] prevTransformNames = pipelineMeta.getPrevTransformNames(transformName);
     if (prevTransformNames != null) {
@@ -106,7 +102,8 @@ public class MultiMergeJoinDialog extends BaseTransformDialog {
       }
     }
 
-    return nameList.toArray(new String[nameList.size()]);
+    joinMeta.setInputTransforms(nameList.toArray(new String[0]));
+    return nameList.toArray(new String[0]);
   }
 
   /*
