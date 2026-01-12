823393853be0a60a980fb3ccd2d51e47ac19c383
Hans Van Akelyen <hans.van.akelyen@gmail.com>
2026-01-12 17:23:58 +0100

    Add option to disable zoom using scrolling, fixes #5160
    spotless

--- ui/src/main/java/org/apache/hop/ui/core/PropsUi.java
+++ ui/src/main/java/org/apache/hop/ui/core/PropsUi.java
@@ -82,6 +82,7 @@ public class PropsUi extends Props {
   private static final String MAX_EXECUTION_LOGGING_TEXT_SIZE = "MaxExecutionLoggingTextSize";
   private static final String GRAPH_EXTRA_VIEW_VERTICAL_ORIENTATION =
       "GraphExtraViewVerticalOrientation";
+  private static final String DISABLE_ZOOM_SCROLLING = "DisableZoomScrolling";
 
   public static final int DEFAULT_MAX_EXECUTION_LOGGING_TEXT_SIZE = 2000000;
   private Map<RGB, RGB> contrastingColors;
@@ -1163,5 +1164,13 @@ public class PropsUi extends Props {
     formLayout.marginBottom = getFormMargin();
     formLayout.marginRight = getFormMargin();
     return formLayout;
+  }
+
+  public boolean isZoomScrollingDisabled() {
+    return YES.equalsIgnoreCase(getProperty(DISABLE_ZOOM_SCROLLING, NO));
+  }
+
+  public void setZoomScrollingDisabled(boolean disabled) {
+    setProperty(DISABLE_ZOOM_SCROLLING, disabled ? YES : NO);
   }
 }
