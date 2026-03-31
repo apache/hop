eb34afb75785afeb6403b1b1f41eb77e56214d87
lance <leehaut@gmail.com>
2026-03-31 17:42:35 +0800

    Replace generic RuntimeException with HopRuntimeException to satisfy Sonar rules
    Signed-off-by: lance <leehaut@gmail.com>

--- core/src/test/java/org/apache/hop/core/exception/HopRuntimeExceptionTests.java
+++ core/src/test/java/org/apache/hop/core/exception/HopRuntimeExceptionTests.java
@@ -18,7 +18,9 @@
 
 package org.apache.hop.core.exception;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
