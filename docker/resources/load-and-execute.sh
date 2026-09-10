4eb159a7e913c563f7a4a9966a55a3ecb29abc0d
mattcasters <mattcasters@gmail.com>
2026-09-10 12:42:22 +0200

    Issue #8299 : Word-split HOP_CONFIG_OPTIONS when invoking hop-conf
    Quoted expansion passed the entire env var to hop-conf.sh as one argument,
    so multiple options from Docker Compose were rejected as unmatched. Convert
    the string to an array (env vars cannot carry bash arrays) and expand it
    like HOP_EXEC_OPTIONS.

--- docker/resources/load-and-execute.sh
+++ docker/resources/load-and-execute.sh
@@ -305,8 +305,11 @@ if [ -n "${HOP_CONFIG_OPTIONS}" ]; then
   # We have a hop-config to run with the given options
   #
   echo "Configuring Hop with : ${HOP_CONFIG_OPTIONS}"
+  # Docker env vars cannot carry a bash array, so word-split into arguments.
+  # shellcheck disable=SC2206
+  HOP_CONFIG_OPTION_ARRAY=(${HOP_CONFIG_OPTIONS})
   "${DEPLOYMENT_PATH}"/hop-conf.sh \
-    "${HOP_CONFIG_OPTIONS}" \
+    "${HOP_CONFIG_OPTION_ARRAY[@]}" \
     2>&1 | tee ${HOP_LOG_PATH}
 fi
 
